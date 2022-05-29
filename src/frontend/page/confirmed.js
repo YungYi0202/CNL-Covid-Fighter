c093ab8bc437c600367f9a2b38c199f912fe35a4
jiyuntu <wendy3925@gmail.com>
2022-05-29 11:40:12 +0800

    add information

--- src/frontend/page/confirmed.js
+++ src/frontend/page/confirmed.js
@@ -36,16 +36,6 @@ const Confirmed = ({ user, setUser, back }) => {
       <Button onClick={handleSubmitClick} size="large" type="primary">
         {" "}submit{" "}
       </Button>
-      <br />
-      <br />
-      <a href="https://www.cdc.gov.tw/Category/MPage/9wonLmQrvAdSAx55Ec7aWw">
-        衛福部確診個案自主回報疫調系統
-      </a>
-      <br />
-      <br />
-      <a href="https://my.ntu.edu.tw/ntuwdc/ConfirmedReport.aspx">
-        臺大確診者通報
-      </a>
     </>
   );
 };
--- src/frontend/page/status.js
+++ src/frontend/page/status.js
@@ -1,18 +1,38 @@
 import React from "react";
-import { Button } from "antd";
+import { Menu, Button, Typography } from "antd";
 import Report from "../page/report";
 import { updateUser } from "../../server/api";
 
+const { Title, Paragraph, Text, Link } = Typography;
+const items = [
+  {
+    label: "PCR/快篩陽性",
+    key: "confirmed"
+  },
+  {
+    label: "入境者",
+    key: "entrant"
+  },
+  {
+    label: "確診者同住親友",
+    key: "is_contact"
+  },
+  {
+    label: "確診者的密切接觸者的接觸者",
+    key: "contact_of_contacts"
+  }
+];
+
 function formatDate(date) {
   var d = new Date(date),
-      month = '' + (d.getMonth() + 1),
-      day = '' + d.getDate(),
-      year = d.getFullYear();
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
 
-  if (month.length < 2) 
-      month = '0' + month;
-  if (day.length < 2) 
-      day = '0' + day;
+  if (month.length < 2)
+    month = '0' + month;
+  if (day.length < 2)
+    day = '0' + day;
 
   return [year, month, day].join('/');
 }
@@ -20,6 +40,7 @@ function formatDate(date) {
 const Status = ({ user, setUser, handleLogoutClick }) => {
   const [report, setReport] = React.useState(false);
   const [status, setStatus] = React.useState("健康");
+  const [information, setInformation] = React.useState("confirmed");
 
   React.useEffect(() => {
     async function renderStatus() {
@@ -108,13 +129,66 @@ const Status = ({ user, setUser, handleLogoutClick }) => {
     setReport(false);
   };
 
+  const onClick = e => {
+    setInformation(e.key);
+  };
+
   return (
     <>
       <h1> 歡迎，{user.username}！你當前的狀態為： {status} </h1>
       {report === true ? (
         <Report user={user} setUser={setUser} back={back} />
       ) : (
-        <Button onClick={handleReportClick}> 我要通報確診 / 密切接觸者 </Button>
+        <>
+          <Button onClick={handleReportClick}> 我要通報確診 / 密切接觸者 </Button>
+          <br />
+          <br />
+          <div style={{ display: "flex" }}>
+            <Menu
+              onClick={onClick}
+              style={{ width: 256 }}
+              defaultSelectedKeys={[information]}
+              mode="inline"
+              items={items}
+            />
+            <Typography>
+              <Paragraph>
+                {information === "confirmed" ? (
+                  <ul>
+                    <li>步驟一：透過遠距或視訊方式由醫師確認快篩結果。</li>
+                    <li>步驟二：至<Link href="https://www.cdc.gov.tw/Category/MPage/9wonLmQrvAdSAx55Ec7aWw">衛福部確診個案自主回報疫調系統</Link>回報。</li>
+                    <li>步驟三：至<Link href="https://my.ntu.edu.tw/ntuwdc/ConfirmedReport.aspx">臺大確診者通報系統</Link>通報。</li>
+                    <li>步驟四：居家照護七天。</li>
+                    <li>步驟五：無需採檢即可解除居家照護。</li>
+                    <li>步驟六：自主健康管理七天。</li>
+                  </ul>
+                ) : information === "entrant" ? (
+                  <ul>
+                    <li>步驟一：至<Link href="https://my.ntu.edu.tw/ntuwdc/reporting.aspx">臺大居家檢疫通報系統</Link>通報。</li>
+                    <li>步驟二：居家檢疫七天。</li>
+                    <li>步驟三：居家檢疫第七天或出現症狀時快篩。</li>
+                    <li>步驟四：自主健康管理七天。</li>
+                  </ul>
+                ) : information === "is_contact" ? (
+                  <ul>
+                    <li>步驟一：至<Link href="https://hackmd.io/https://my.ntu.edu.tw/ntuwdc/internalReport1.aspx">臺大密切接觸者通報</Link>通報。</li>
+                    如果未打三劑：
+                    <li>步驟二：居家隔離三天，並且在此期間進行一次快篩。</li>
+                    <li>步驟三：四天自主防疫。</li>
+                    如果打滿三劑：
+                    <li>步驟二：七天的自主防疫。</li>
+                  </ul>
+                ) : information === "contact_of_contacts" ? (
+                  <ul>
+                    <li>步驟一：自我健康監測七天。</li>
+                  </ul>
+                ) : (
+                  <></>
+                )}
+              </Paragraph>
+            </Typography>
+          </div>
+        </>
       )}
       <br />
       <br />
